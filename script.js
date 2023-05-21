e3c911edcc28e4cc407e70f1f484ac38b8a314eb
parsvensson <par.svensson@gmail.com>
2023-05-21 11:58:41 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+// Get the video element
+const video = document.querySelector('#webcam');
+
+if (navigator.mediaDevices.getUserMedia) {
+    // Define constraints for video
+    const constraints = { video: true };
+
+    // Get media with the video constraints
+    navigator.mediaDevices.getUserMedia(constraints)
+    .then((stream) => {
+        // Set video source to the stream from the webcam
+        video.srcObject = stream;
+    })
+    .catch((err) => {
+        console.log(`Error: ${err}`);
+    });
+}
