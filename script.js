57f03dc23ba95afb3f0157aab5ac966e7c6a569e
parsvensson <par.svensson@gmail.com>
2023-05-21 12:07:54 +0200

    Changed connection to the webcam on DOMContentLoaded

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-// Get the video element
-const video = document.querySelector('#webcam');
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the video element
+    const video = document.querySelector('#webcam');
 
-if (navigator.mediaDevices.getUserMedia) {
-    // Define constraints for video
-    const constraints = { video: true };
+    if (navigator.mediaDevices.getUserMedia) {
+        // Define constraints for video
+        const constraints = { video: true };
 
-    // Get media with the video constraints
-    navigator.mediaDevices.getUserMedia(constraints)
-    .then((stream) => {
-        // Set video source to the stream from the webcam
-        video.srcObject = stream;
-    })
-    .catch((err) => {
-        console.log(`Error: ${err}`);
-    });
-}
+        // Get media with the video constraints
+        navigator.mediaDevices.getUserMedia(constraints)
+        .then((stream) => {
+            // Set video source to the stream from the webcam
+            video.srcObject = stream;
+        })
+        .catch((err) => {
+            console.log(`Error: ${err}`);
+        });
+    }
+});
