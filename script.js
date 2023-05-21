0cd796ce576d8d53f93f4d0671205effba539385
parsvensson <par.svensson@gmail.com>
2023-05-21 12:22:36 +0200

    Connected webcam to model and started to do predictions.

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-document.addEventListener("DOMContentLoaded", function() {
+// Wait for the DOM content to finish loading
+document.addEventListener("DOMContentLoaded", async function() {
     // Get the video element
     const video = document.querySelector('#webcam');
+
+    // Load the model
+    const model = await tf.loadLayersModel('model/model.json');
 
     if (navigator.mediaDevices.getUserMedia) {
         // Define constraints for video
@@ -11,6 +15,26 @@ document.addEventListener("DOMContentLoaded", function() {
         .then((stream) => {
             // Set video source to the stream from the webcam
             video.srcObject = stream;
+
+            // Wait for the video to start playing
+            video.onloadedmetadata = () => {
+                video.play();
+
+                // Run inference on the video
+                setInterval(async () => {
+                    // Convert the video frame to a tensor                  
+                    const img = tf.browser.fromPixels(video);
+                    const resizedImg = img.resizeNearestNeighbor([160, 160]).toFloat();
+                    const normalizedImg = resizedImg.div(tf.scalar(127.5)).sub(tf.scalar(1));
+                    const batchedImg = normalizedImg.expandDims(0);
+
+                    // Make a prediction
+                    const prediction = model.predict(batchedImg);
+
+                    // Log the prediction
+                    prediction.print();
+                }, 1000);
+            };
         })
         .catch((err) => {
             console.log(`Error: ${err}`);
